0387b030a6d74d7f6a699902658ae1b15c75238b
shubham-mehta-002 <134730928+shubham-mehta-002@users.noreply.github.com>
2024-04-29 19:09:21 +0530

    Add files via upload

---
+++ src/pages/HomePage/genderData.jsx
@@ -0,0 +1,24 @@
+import { v4 as uuid } from 'uuid';
+export const genderData=[
+    {        
+        id:uuid(),
+        name:"Female",
+        set:"w",
+        src:"https://res.cloudinary.com/dimqqgecs/image/upload/v1712558946/hnhekxjnrw0qskunjcdc.png"
+    },
+    {        
+        id:uuid(),
+        name:"Male",
+        set:"m",
+        src:"https://res.cloudinary.com/dimqqgecs/image/upload/v1712558946/a5umqujseh1uy2ftbggu.png"
+    },
+
+    {        
+        id:uuid(),
+        name:"Kids",
+        set:"k",
+        src:"https://res.cloudinary.com/dimqqgecs/image/upload/v1712558947/kvecwpgj2hhqzqsy7jjc.png"
+    },
+]
+
+export default genderData
